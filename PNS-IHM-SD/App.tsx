055ba5a61321e624f3b502d22fc5889a1b7308bd
Aldric-Ducreux <neogeekmo@live.fr>
2020-01-09 10:54:51 +0100

    #1 mise en place de la map sur Paris
    Carte mise en place sur Paris centrer sur les sites des JO, zoom désactivé pour le moment
    Rajout du code de géolocalisation (non utiliser encore)

--- PNS-IHM-SD/App.tsx
+++ PNS-IHM-SD/App.tsx
@@ -1,16 +1,72 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React, { Component } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { Platform,Text, StyleSheet, View, Dimensions} from 'react-native';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView style={styles.mapStyle} />
-      </View>
-    );
+export default class App extends Component {  
+  state = {
+    location: null,
+    errorMessage: null,
+  };
+
+  UNSAFE_componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
   }
-}
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+
+  render() {  
+    let text = 'Waiting..';
+    let myLatitude, myLongitude;
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+      myLatitude = this.state.location.coords.latitude;
+      myLongitude = this.state.location.coords.longitude;
+
+    }
+    return (  
+      <View style={styles.container}>  
+        <MapView  style={styles.mapStyle}  
+          showsUserLocation={true}  
+          zoomEnabled={true}  
+          zoomControlEnabled={false}  
+          initialRegion={{  
+            latitude: 48.901411,   
+            longitude: 2.330379,  
+            latitudeDelta: 0.25,  
+            longitudeDelta: 0.25,  
+          }}>  
+  
+          <Marker  
+            coordinate={{ latitude: 28.579660, longitude: 77.321110 }}  
+            title={"JavaTpoint"}  
+            description={"Java Training Institute"}  
+          />  
+        </MapView>  
+          
+      </View>  
+    );  
+  }  
+}  
 
 const styles = StyleSheet.create({
   container: {
@@ -23,4 +79,9 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
+  paragraph: {
+    marginTop: 500,
+    fontSize: 18,
+    textAlign: 'center',
+  }
 });
