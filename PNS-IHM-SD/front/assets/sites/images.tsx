7dc6466b4f3bf2909f787865f0703b3648d88e99
Aldric-Ducreux <neogeekmo@live.fr>
2020-01-20 20:14:22 +0100

    #21 update image libre de droit

--- PNS-IHM-SD/front/assets/sites/images.tsx
+++ PNS-IHM-SD/front/assets/sites/images.tsx
@@ -10,6 +10,16 @@ const images ={
     baggage_claim: require('./../imagesScript/baggage_claim.jpg'),
     dining_table: require('./../imagesScript/dining_table.jpg'),
     living_room: require('./../imagesScript/living_room.jpg'),
+    polytech1: require('./../imagesScript/scene/polytech1.jpg'),
+    polytech2: require('./../imagesScript/scene/polytech2.jpg'),
+    polytech3: require('./../imagesScript/scene/polytech3.jpg'),
+    polytech4: require('./../imagesScript/scene/polytech4.jpg'),
+    polytech5: require('./../imagesScript/scene/polytech5.jpg'),
+    polytech6: require('./../imagesScript/scene/polytech6.jpg'),
+    polytech7: require('./../imagesScript/scene/polytech7.jpg'),
+    polytech8: require('./../imagesScript/scene/polytech8.jpg'),
+    polytech9: require('./../imagesScript/scene/polytech9.jpg'),
+    polytech10: require('./../imagesScript/scene/polytech10.jpg')
 }
 
 export default images;
